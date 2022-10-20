e17a4c9a968b615c013e4491b4ac4e122a062917
hunzsig <mzyhaohaoren@qq.com>
2022-10-20 13:37:10 +0800

    book(*): update

--- src/book/singluar/zh_tw/_js/summary.js
+++ src/book/singluar/zh_tw/_js/summary.js
@@ -51,22 +51,22 @@ import {
 // only one layout
 export default [
   {icon: <ReadOutlined/>, key: 'index', label: "快速開始"},
-  {icon: <ApartmentOutlined/>, key: 'struct', label: "項目結構"},
+  {icon: <ApartmentOutlined/>, key: 'struct', label: "專案結構"},
   {icon: <ToolOutlined/>, key: 'cmd', label: "命令工具"},
   {icon: <ExceptionOutlined/>, key: 'update', label: "更新日誌"},
-  {icon: <BugOutlined/>, key: 'debug', label: "調試打印"},
-  {icon: <VideoCameraOutlined/>, key: 'video', label: "視頻演示"},
+  {icon: <BugOutlined/>, key: 'debug', label: "除錯列印"},
+  {icon: <VideoCameraOutlined/>, key: 'video', label: "影片演示"},
   {
     icon: <FunctionOutlined/>, key: 'library', label: "功能庫", children: [
       {icon: <AaOutline/>, key: 'assets', label: "資産"},
       {icon: <NodeIndexOutlined/>, key: 'bj', label: "BJ"},
       {icon: <NodeIndexOutlined/>, key: 'cj', label: "CJ"},
       {icon: <NodeIndexOutlined/>, key: 'japi', label: "JAPI(SL)"},
-      {icon: <FormatPainterOutlined/>, key: 'colour', label: "文本顔色"},
-      {icon: <AppstoreOutlined/>, key: 'object', label: "對象"},
+      {icon: <FormatPainterOutlined/>, key: 'colour', label: "文字顔色"},
+      {icon: <AppstoreOutlined/>, key: 'object', label: "物件"},
       {icon: <HighlightOutlined/>, key: 'modify', label: "修改器"},
       {icon: <SyncOutlined/>, key: 'sync', label: "同步"},
-      {icon: <RetweetOutlined/>, key: 'async', label: "異步"},
+      {icon: <RetweetOutlined/>, key: 'async', label: "非同步"},
     ]
   },
   {
@@ -90,19 +90,19 @@ export default [
       {icon: <SoundOutlined/>, key: 'sound', label: "聲效"},
       {icon: <ClockCircleOutlined/>, key: 'timer', label: "計時器"},
       {icon: <DollarCircleOutlined/>, key: 'worth', label: "層級資源"},
-      {icon: <MenuUnfoldOutlined/>, key: 'dialog', label: "難度選擇對話框"},
+      {icon: <MenuUnfoldOutlined/>, key: 'dialog', label: "難度選擇對話方塊"},
       {icon: <StockOutlined/>, key: 'orderRoute', label: "路線藍圖"},
     ]
   },
   {
     icon: <QuestionOutlined/>, key: 'other', label: "其他學習", children: [
-      {icon: <SetOutline/>, key: 'jetbrain', label: "Jetbrain設置"},
+      {icon: <SetOutline/>, key: 'jetbrain', label: "Jetbrain設定"},
       {icon: <SmileOutline/>, key: 'q_a', label: "詢問的技巧"},
       {icon: <CloudUploadOutlined/>, key: 'dz', label: "平臺上線須知"},
       {icon: <KoubeiOutline/>, key: 'war3_tec', label: "魔獸作圖小技巧"},
       {icon: <PictureOutline/>, key: 'war3_terrainArt', label: "魔獸地形貼圖路徑"},
       {icon: <ExclamationShieldOutline/>, key: 'war3_crash', label: "魔獸常見崩潰原因"},
-      {icon: <BoxPlotOutlined/>, key: 'war3_func', label: "魔獸函數異同特徵"},
+      {icon: <BoxPlotOutlined/>, key: 'war3_func', label: "魔獸函式異同特徵"},
       {icon: <TransportQRcodeOutline/>, key: 'lua_engine', label: "YDLua引擎"},
       {icon: <BankcardOutline/>, key: 'fdf', label: "FDF"},
     ]
