083581406382c680925d0f95fc9c78840bebd123
hunzsig <mzyhaohaoren@qq.com>
2022-12-14 11:59:47 +0800

    book(*): update

--- src/preprocessing/index.js
+++ src/preprocessing/index.js
@@ -12,15 +12,15 @@ import router from "./router.js";
  * setting可以设 function _promise() { return new Promise... } 或值
  */
 export default {
-  book: "singluar", // 选一本书
+  book: "lik", // 选一本书
   // title: "HunZsig", // title 标题，默认 <book>技术文档 · 魂书
   // cover: Www, // 封面(Composer)
   router: router,
   lang: [
     {key: "zh_cn", label: "简体中文",},
-    // {key: "zh_hk", label: "香港繁體",},
-    // {key: "zh_tw", label: "臺灣繁體",},
-    // {key: "en_us", label: "English",},
-    // {key: "ja_jp", label: "日本語",},
+    {key: "zh_hk", label: "香港繁體",},
+    {key: "zh_tw", label: "臺灣繁體",},
+    {key: "en_us", label: "English",},
+    {key: "ja_jp", label: "日本語",},
   ]
 }
